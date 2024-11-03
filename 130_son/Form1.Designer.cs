9e22dbae2c5a2dce88608c41e854313338554b11
Nuri Işık <nurii6345@gmail.com>
2024-11-03 13:38:41 +0300

    #

---
+++ 130_son/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace _130_son
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_adsyoad = new System.Windows.Forms.TextBox();
+            this.button_yenikisi = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button_ekle = new System.Windows.Forms.Button();
+            this.button_sil = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button_bul = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button_arayaekle = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox_meslek = new System.Windows.Forms.ComboBox();
+            this.comboBox_birim = new System.Windows.Forms.ComboBox();
+            this.label_aranan = new System.Windows.Forms.Label();
+            this.textBox_aranan = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adı- soyadı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mesleği";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Çalıştığı Birim";
+            // 
+            // textBox_adsyoad
+            // 
+            this.textBox_adsyoad.Location = new System.Drawing.Point(163, 33);
+            this.textBox_adsyoad.Name = "textBox_adsyoad";
+            this.textBox_adsyoad.Size = new System.Drawing.Size(121, 22);
+            this.textBox_adsyoad.TabIndex = 4;
+            // 
+            // button_yenikisi
+            // 
+            this.button_yenikisi.Location = new System.Drawing.Point(89, 163);
+            this.button_yenikisi.Name = "button_yenikisi";
+            this.button_yenikisi.Size = new System.Drawing.Size(97, 30);
+            this.button_yenikisi.TabIndex = 8;
+            this.button_yenikisi.Text = "Yeni Kişi";
+            this.button_yenikisi.UseVisualStyleBackColor = true;
+            this.button_yenikisi.Click += new System.EventHandler(this.button_yenikisi_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox_birim);
+            this.groupBox1.Controls.Add(this.comboBox_meslek);
+            this.groupBox1.Controls.Add(this.button_yenikisi);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox_adsyoad);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(304, 217);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Personel";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button_sil);
+            this.groupBox2.Controls.Add(this.button_ekle);
+            this.groupBox2.Location = new System.Drawing.Point(440, 18);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(235, 82);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "İşlemler";
+            // 
+            // button_ekle
+            // 
+            this.button_ekle.Location = new System.Drawing.Point(6, 27);
+            this.button_ekle.Name = "button_ekle";
+            this.button_ekle.Size = new System.Drawing.Size(94, 37);
+            this.button_ekle.TabIndex = 0;
+            this.button_ekle.Text = "Ekle";
+            this.button_ekle.UseVisualStyleBackColor = true;
+            this.button_ekle.Click += new System.EventHandler(this.button_ekle_Click);
+            // 
+            // button_sil
+            // 
+            this.button_sil.Location = new System.Drawing.Point(136, 27);
+            this.button_sil.Name = "button_sil";
+            this.button_sil.Size = new System.Drawing.Size(93, 37);
+            this.button_sil.TabIndex = 1;
+            this.button_sil.Text = "Sil";
+            this.button_sil.UseVisualStyleBackColor = true;
+            this.button_sil.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button_arayaekle);
+            this.groupBox3.Controls.Add(this.button6);
+            this.groupBox3.Controls.Add(this.button5);
+            this.groupBox3.Controls.Add(this.button_bul);
+            this.groupBox3.Location = new System.Drawing.Point(440, 129);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(229, 100);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "İşlemler";
+            // 
+            // button_bul
+            // 
+            this.button_bul.Location = new System.Drawing.Point(11, 18);
+            this.button_bul.Name = "button_bul";
+            this.button_bul.Size = new System.Drawing.Size(89, 23);
+            this.button_bul.TabIndex = 0;
+            this.button_bul.Text = "Bul";
+            this.button_bul.UseVisualStyleBackColor = true;
+            this.button_bul.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(122, 18);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(89, 23);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Sonraki";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(11, 71);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(89, 23);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "Değiştir";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button_arayaekle
+            // 
+            this.button_arayaekle.Location = new System.Drawing.Point(122, 71);
+            this.button_arayaekle.Name = "button_arayaekle";
+            this.button_arayaekle.Size = new System.Drawing.Size(89, 23);
+            this.button_arayaekle.TabIndex = 3;
+            this.button_arayaekle.Text = "Araya Ekle";
+            this.button_arayaekle.UseVisualStyleBackColor = true;
+            this.button_arayaekle.Click += new System.EventHandler(this.button_arayaekle_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 235);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(221, 148);
+            this.listBox1.TabIndex = 11;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listbox_secim);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(287, 235);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(221, 148);
+            this.listBox2.TabIndex = 12;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listbox_secim);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(562, 235);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(221, 148);
+            this.listBox3.TabIndex = 13;
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listbox_secim);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 415);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 16);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Personel Sayısı";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(189, 415);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(588, 415);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 16);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Sıra No";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(656, 415);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 16);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "label7";
+            // 
+            // comboBox_meslek
+            // 
+            this.comboBox_meslek.FormattingEnabled = true;
+            this.comboBox_meslek.Location = new System.Drawing.Point(163, 84);
+            this.comboBox_meslek.Name = "comboBox_meslek";
+            this.comboBox_meslek.Size = new System.Drawing.Size(121, 24);
+            this.comboBox_meslek.TabIndex = 9;
+            // 
+            // comboBox_birim
+            // 
+            this.comboBox_birim.FormattingEnabled = true;
+            this.comboBox_birim.Location = new System.Drawing.Point(163, 127);
+            this.comboBox_birim.Name = "comboBox_birim";
+            this.comboBox_birim.Size = new System.Drawing.Size(121, 24);
+            this.comboBox_birim.TabIndex = 10;
+            // 
+            // label_aranan
+            // 
+            this.label_aranan.AutoSize = true;
+            this.label_aranan.Location = new System.Drawing.Point(475, 112);
+            this.label_aranan.Name = "label_aranan";
+            this.label_aranan.Size = new System.Drawing.Size(56, 16);
+            this.label_aranan.TabIndex = 4;
+            this.label_aranan.Text = "Aranan :";
+            // 
+            // textBox_aranan
+            // 
+            this.textBox_aranan.Location = new System.Drawing.Point(551, 109);
+            this.textBox_aranan.Name = "textBox_aranan";
+            this.textBox_aranan.Size = new System.Drawing.Size(100, 22);
+            this.textBox_aranan.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_aranan);
+            this.Controls.Add(this.textBox_aranan);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_adsyoad;
+        private System.Windows.Forms.Button button_yenikisi;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button_sil;
+        private System.Windows.Forms.Button button_ekle;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button_arayaekle;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button_bul;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox_birim;
+        private System.Windows.Forms.ComboBox comboBox_meslek;
+        private System.Windows.Forms.Label label_aranan;
+        private System.Windows.Forms.TextBox textBox_aranan;
+    }
+}
+
---
+++ 130_son/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _130_son
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //program açılırken
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] meslekler =
+            {
+                "Mühendis","Öğretmen","Öğrenci","İşçi",
+                "Programcı","Muhasebeci","Satış","Pazarlama",
+                "Ekonomist","Müfettiş","Avukat","Mimar"
+            };
+            comboBox_meslek.Items.AddRange(meslekler);
+
+            string[] birimler =
+          {
+                "Satış ","Eğitim","Pazarlama","Reklam",
+                "Üretim","Muhasebe","Teknik Destek","Yazılım",
+                "NEtwork","Yönetim","Çağrı Merkezi"
+            };
+            comboBox_birim.Items.AddRange(birimler);
+
+            try
+            {
+                //dosya ac
+                System.IO.TextReader dosya_ac = System.IO.File.OpenText("C:\\Users\\nurii\\OneDrive\\Desktop\\unsinDosya2");
+                string satir;
+                
+                //dosyadaki bilgileri listbox lara attık
+                while ((satir=dosya_ac.ReadLine())!=null)
+                {
+                    listBox1.Items.Add(satir);
+
+                    satir=dosya_ac.ReadLine();
+                    listBox2.Items.Add(satir);
+
+                    satir = dosya_ac.ReadLine();
+                    listBox3.Items.Add(satir);
+
+                }
+                dosya_ac.Close();
+            }
+            catch 
+            {
+                //Program ilk çalıştırıldığında dosya da hiç veri olmayacağı için cathch blokları çalışacak
+                //Ancak burda herhangi bir işlem yapılmayacak.
+                
+            }
+            label5.Text = listBox1.Items.Count.ToString();
+            label7.Text=(listBox1.SelectedIndex+1).ToString();
+        }
+
+        //program kapatılırken
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //dosya oluşturulur
+            System.IO.TextWriter dosya_kaydet = System.IO.File.CreateText("C:\\Users\\nurii\\OneDrive\\Desktop\\unsinDosya2");
+            
+            //listelerdeeki bilgiler for döngüsü yardımı ile dosyaya atılıır.
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                dosya_kaydet.WriteLine(listBox1.Items[i]);
+                dosya_kaydet.WriteLine(listBox2.Items[i]);  
+                dosya_kaydet.WriteLine(listBox3.Items[i]);
+
+            }
+            dosya_kaydet.Close();
+
+        }
+
+        //listboxlardan herhangi bir öge seçildiğinde ilişkili(aynı indis) ögelerde seçilmesini sağlayan metod
+        //bu metodu tüm listBoxların SelectIndexChanced propertiesine ekeledim
+        private void listbox_secim (object sender, System.EventArgs e)
+        {
+            int indis;
+            indis=(sender as ListBox).SelectedIndex;
+
+            listBox1.SelectedIndex= indis;
+            listBox2.SelectedIndex=indis;
+            listBox3.SelectedIndex=indis;
+            //listbox kontrolleri çok uzunsa kaydırma çubukları gelecektir
+            //bu durumda listeler aynı hizada olmayabilr
+            //bunun için aynı hizaya getirilir
+            int top_indis;
+            top_indis = (sender as ListBox).TopIndex;
+            listBox1.TopIndex= top_indis;
+            listBox2.TopIndex= top_indis;
+            listBox3.TopIndex= top_indis;
+            //listelerde seçili olan ögelerim textbox ve combobox üzerinde gösterilir
+            textBox_adsyoad.Text=listBox1.Text;
+            comboBox_meslek.Text=listBox2.Text;
+            comboBox_birim.Text=listBox3.Text;
+
+            label7.Text=(listBox1.SelectedIndex+1).ToString();
+
+        }
+
+        private void button_ekle_Click(object sender, EventArgs e)
+        {
+            int yer;
+            yer = listBox1.FindStringExact(textBox_adsyoad.Text); //yazılan ad soyadı listede ara
+            if(yer<0) //yoksa
+            {
+                listBox1.Items.Add(textBox_adsyoad.Text);
+                listBox2.Items.Add(comboBox_meslek);
+                listBox3.Items.Add(comboBox_birim); 
+                
+                label5.Text=listBox1.Items.Count.ToString();
+
+            }
+            else
+            {
+                listBox1.SelectedIndex = yer;//bulunan kayıt
+                MessageBox.Show("bu eleman listede zaten var");
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedIndex<0)
+            {
+                MessageBox.Show("önce silecenek elemanı seçiniz");
+                return;
+            }
+
+            int indis=listBox1.SelectedIndex;
+            DialogResult c;
+            c=MessageBox.Show(listBox1.SelectedItem+"silinsin mi?","silme",MessageBoxButtons.YesNo);
+            if(c==DialogResult.Yes)
+            {
+                listBox1.Items.RemoveAt(indis);
+                listBox2.Items.RemoveAt(indis);
+                listBox3.Items.RemoveAt(indis); 
+
+                label5.Text=listBox1.Items.Count.ToString();  
+                
+            }
+
+
+
+        }
+        //arama işleminin sürekli olarak en son kalan yerden devam edebilmesi için 
+        //aranan kelimenin bulunduğu yer bilgileri tutuluyor.
+        //onun için yer bilgisi global tanımlanıyor
+        int yer = 1;
+        string aranan;
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            aranan=textBox_aranan.Text;
+            yer = listBox1.FindStringExact(aranan);
+
+            if (yer < 0)
+            {
+                //bulunamazsa bir benzerini ara
+                yer = listBox1.FindString(aranan);
+
+                if (yer < 0)
+                    MessageBox.Show("aranan değer bulunamadı");
+
+            }
+            else //bulunursa
+            {
+                listBox1.SelectedIndex = yer;
+            }
+
+
+
+              
+
+            
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (yer < listBox1.Items.Count - 1)
+            {
+                yer = listBox1.FindString(aranan, yer);
+
+                if(yer>=0)
+                {
+                    listBox1.SelectedIndex=yer;
+                }
+
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int indis;
+            string adsoyad, meslek, birim;
+
+            adsoyad=textBox_adsyoad.Text;
+            meslek=comboBox_meslek.Text;
+            birim=comboBox_birim.Text;
+
+            indis = listBox1.SelectedIndex;
+            if (indis < 0)
+            {
+                MessageBox.Show("önce değiştirilecek kişiyi seçin");
+            }
+            else
+            {
+                listBox1.Items[indis] = adsoyad;
+                listBox2.Items[indis]=meslek;
+                listBox3.Items[indis] = birim;
+            }
+        }
+
+        private void button_arayaekle_Click(object sender, EventArgs e)
+        {
+            int indis;
+            indis=listBox1.SelectedIndex;
+
+            if (indis < 0)
+            {
+                MessageBox.Show("önce elemanın nereye ekeleneceğini seçiniz:");
+            }
+            else
+            {
+                int yer;
+                yer=listBox1.FindStringExact(textBox_adsyoad.Text);
+
+                if (yer < 0)
+                {
+                    listBox1.Items.Insert(indis, textBox_adsyoad);
+                    listBox2.Items.Insert(indis,comboBox_meslek);
+                    listBox3.Items.Insert(indis,comboBox_birim);
+
+                    label5.Text=listBox1.Items.Count.ToString();
+
+                }
+                else
+                {
+                    listBox1.SelectedIndex = yer;
+                    MessageBox.Show("bu eleman zaten var");
+                }
+
+            }
+        }
+
+        private void button_yenikisi_Click(object sender, EventArgs e)
+        {
+            textBox_adsyoad.Text = "";
+            comboBox_meslek.Text = "";
+            comboBox_birim.Text = "";
+        }
+    }
+}
